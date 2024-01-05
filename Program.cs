e67b6070755b70c0b8ea4b6e1195a9ea63644534
Tony <antony.branquinho@gmail.com>
2024-01-05 10:58:26 -0300

    basico

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro");
+            int numero = int.Parse(Console.ReadLine());
+
+            string saida = "nulo"; 
+            if (numero >= 0) {
+                saida = "nao é negativo";        
+            }
+            else {
+                saida = "negativo";
+            }
+
+            Console.WriteLine(saida);
+            Console.WriteLine();
+        }
+    }
+}
